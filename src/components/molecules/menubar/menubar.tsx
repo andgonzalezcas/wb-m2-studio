fc2eabfae27988c96cb251e53b2d822e30284aed
andgonzalezcas <andgonzalezcas@unal.edu.co>
2025-03-30 16:02:26 -0500

    feat(app): add swap lang on menu bar

--- src/components/molecules/menubar/menubar.tsx
+++ src/components/molecules/menubar/menubar.tsx
@@ -1,11 +1,27 @@
 import { Sections } from "@/enums/global";
 import { handleNavigation } from "@/utils/common";
+import { useLingui } from "@lingui/react/macro";
+import { MouseEvent } from "react";
+import en from "@/locale/en.json";
+import es from "@/locale/es.json";
 
 interface props {
     isDark: boolean
 }
 
+const messages = { en, es };
+
 const Menubar = ({ isDark }: props) => {
+    const { i18n } = useLingui();
+
+    const toggleLanguage = (e: MouseEvent) => {
+        e.stopPropagation();
+        const newLang = i18n.locale === "es" ? "en" : "es";
+        i18n.load(newLang, messages[newLang]);
+        i18n.activate(newLang);
+        document.documentElement.lang = newLang;
+    };
+
     return (
         <div
             onClick={() => handleNavigation(Sections.INTRODUCTION)}
@@ -15,6 +31,10 @@ const Menubar = ({ isDark }: props) => {
                 <div className={`w-7 h-0 border bg-white ${isDark ? "border-[#232C33]" : "border-white"}`} />
                 <div className={`w-4 h-0 border bg-white ${isDark ? "border-[#232C33]" : "border-white"}`} />
             </section>
+
+            <button onClick={toggleLanguage} className={`rounded text-2xl font-extralight ${isDark ? "text-[#232C33]" : "text-white"}`}>
+                {i18n.locale === "es" ? "ES" : "EN"}
+            </button>
         </div>
     )
 }
--- src/layout/i18nProvider.tsx
+++ src/layout/i18nProvider.tsx
@@ -8,14 +8,14 @@ const messages = { en, es };
 const getUserLocale = () => (navigator.language.startsWith("es") ? "es" : "en");
 
 const loadLocale = (locale: "en" | "es") => {
+    locale = "es";
     document.documentElement.lang = locale;
     i18n.load(locale, messages[locale]);
     i18n.activate(locale);
 };
 
 const I18nWrapper = ({ children }: { children: React.ReactNode }) => {
-    // const locale = getUserLocale();
-    const locale = "es";
+    const locale = getUserLocale();
     loadLocale(locale);
 
     return <I18nProvider i18n={i18n}>{children}</I18nProvider>;
