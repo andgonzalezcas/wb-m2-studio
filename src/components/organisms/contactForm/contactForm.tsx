d201d5a7ff9d5fd21cb44070e225ab48f9f004e6
andgonzalezcas <andgonzalezcas@unal.edu.co>
2025-03-30 15:26:14 -0500

    feat(app): add on contact us form section lingui

--- src/components/organisms/contactForm/contactForm.tsx
+++ src/components/organisms/contactForm/contactForm.tsx
@@ -1,6 +1,7 @@
 import ImageGetter from "@/assets/ImageGetter";
 import LinkButton from "@/components/atoms/linkButton";
 import { Sections } from "@/enums/global";
+import { useLingui } from "@lingui/react/macro";
 import { HTMLInputTypeAttribute } from "react";
 
 const FormInput = (props: {
@@ -19,16 +20,18 @@ const FormInput = (props: {
 }
 
 const ContactForm = () => {
+    const { i18n: t } = useLingui();
+
     return (
         <div className="w-full bg-white xl:px-[93px] relative xl:border-t flex flex-col xl:flex-wrap mb-40 pt-20" id={Sections.CONTACT}>
             <div className="w-full xl:w-1/2 p-10 flex flex-col gap-16 xl:hidden py-32">
                 <LinkButton
-                    label="Formulario si quieres hacer una reforma"
+                    label={t._("contact.renovation_form_label")}
                     link="https://docs.google.com/forms/d/e/1FAIpQLSfLcXSx5AaZmnadgiXqSa7khF8MPY_twQFxvrFRSINb2Gcm4Q/viewform?fbclid=PAY2xjawHpPDdleHRuA2FlbQIxMAABprhOgcBh-RTfaD0_vUsKlInFQgsGVmd2jJ0AdH0PBvLQYHrHbJelQzz3Kg_aem_bnD7s9MjQgIGqrI36_vBgQ"
                     border
                 />
                 <LinkButton
-                    label="Formulario para profesionales colaboradores"
+                    label={t._("contact.professional_form_label")}
                     link="https://docs.google.com/forms/d/e/1FAIpQLSc9YSzgaY-ZI7l4NMwmUQEM7D4yhbQAG_B3uQfRP-3DNXzhlA/viewform?fbclid=PAY2xjawHpPEVleHRuA2FlbQIxMAABpq7kqUcThqG-LM7HZfiXiNr0BS_l6KaJN22kfMqCXl6vulheAqoy5K0lPA_aem_wyxwVmto7jhnKlVkfjicPQ"
                     border
                 />
@@ -46,41 +49,41 @@ const ContactForm = () => {
                 <form className="w-full xl:w-1/2 flex flex-col gap-4 p-10">
                     <FormInput
                         type="text"
-                        placeholder="Nombre"
+                        placeholder={t._("contact.name_placeholder")}
                         required
                     />
                     <FormInput
                         type="email"
-                        placeholder="Email"
+                        placeholder={t._("contact.email_placeholder")}
                         required
                     />
                     <FormInput
                         type="tel"
-                        placeholder="Telefono"
+                        placeholder={t._("contact.phone_placeholder")}
                         required
                     />
                     <textarea
-                        placeholder="Mensaje"
+                        placeholder={t._("contact.message_placeholder")}
                         className="border-[#DCDDDF] border text-2xl font-light px-2 py-1 w-full xl:w-[80%] h-60"
                     />
                     <div className="w-full xl:w-[80%] text-left flex gap-2 px-2">
                         <input type="checkbox" required />
-                        <label className="font-extralight">He leído y acepto los términos y condiciones de uso</label>
+                        <label className="font-extralight">{t._("contact.terms_label")}</label>
                     </div>
                     <div className="w-full xl:w-[80%] flex justify-center">
                         <button className="mt-4 py-1 px-4 border-[#232C3340] border rounded-full w-fit">
-                            Enviar
+                            {t._("contact.send_button")}
                         </button>
                     </div>
                 </form>
                 <div className="w-1/2 p-10 flex-col gap-16 hidden xl:flex">
                     <LinkButton
-                        label="Formulario si quieres hacer una reforma"
+                        label={t._("contact.renovation_form_label")}
                         link="https://docs.google.com/forms/d/e/1FAIpQLSfLcXSx5AaZmnadgiXqSa7khF8MPY_twQFxvrFRSINb2Gcm4Q/viewform?fbclid=PAY2xjawHpPDdleHRuA2FlbQIxMAABprhOgcBh-RTfaD0_vUsKlInFQgsGVmd2jJ0AdH0PBvLQYHrHbJelQzz3Kg_aem_bnD7s9MjQgIGqrI36_vBgQ"
                         border
                     />
                     <LinkButton
-                        label="Formulario para profesionales colaboradores"
+                        label={t._("contact.professional_form_label")}
                         link="https://docs.google.com/forms/d/e/1FAIpQLSc9YSzgaY-ZI7l4NMwmUQEM7D4yhbQAG_B3uQfRP-3DNXzhlA/viewform?fbclid=PAY2xjawHpPEVleHRuA2FlbQIxMAABpq7kqUcThqG-LM7HZfiXiNr0BS_l6KaJN22kfMqCXl6vulheAqoy5K0lPA_aem_wyxwVmto7jhnKlVkfjicPQ"
                         border
                     />
