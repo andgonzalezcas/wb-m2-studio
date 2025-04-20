a3932a08861fd49865aca522606a6e1817d7ed95
andgonzalezcas <andgonzalezcas@unal.edu.co>
2025-04-20 16:34:53 -0500

    feat(app): update professional form to use google form

--- src/components/organisms/Forms/professionalForm.tsx
+++ src/components/organisms/Forms/professionalForm.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import FormInput from "@/components/atoms/formInput"; // Asumo que este es tu input controlado
 import { useLanguage } from "@/hooks/useLanguage";
 import FieldSetInput from "@/components/atoms/fielsetInput";
+import { toast } from "react-toastify";
+import { handleWhatsAppClick } from "@/utils/whatsapp";
 
 interface CollaboratorsFormData {
     name: string;
@@ -9,7 +11,7 @@ interface CollaboratorsFormData {
     phone: string;
     profile: string;
     company: string;
-    collaborators: string[];
+    collaborators: string;
     otherCollaborator: string;
     city: string;
     postalCode: string;
@@ -25,7 +27,7 @@ const ProfessionalForm = () => {
         phone: "",
         profile: "",
         company: "",
-        collaborators: [],
+        collaborators: "",
         otherCollaborator: "",
         city: "",
         postalCode: "",
@@ -33,23 +35,67 @@ const ProfessionalForm = () => {
         terms: false,
     });
 
-    const toggleArrayValue = (value: string) => {
-        setFormData((prev) => {
-            const updatedArray = prev.collaborators.includes(value)
-                ? prev.collaborators.filter((v) => v !== value)
-                : [...prev.collaborators, value];
-            return { ...prev, collaborators: updatedArray };
-        });
-    };
-
     const handleChange = (key: keyof CollaboratorsFormData, value: string | boolean) => {
         setFormData((prev) => ({ ...prev, [key]: value }));
     };
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        console.log(formData);
-        alert("Formulario enviado (simulado).");
+
+        const googleFormUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSddE4xStIA4d6jfJO_uVXfLKLTdTbHjTp1zf2UvIWKryIamwg/formResponse';
+
+        const form = new FormData();
+        form.append('entry.2005620554', formData.name);
+        form.append('entry.8064698', formData.profile);
+        form.append('entry.1360669804', formData.company);
+        form.append('entry.383973379', formData.city);
+        form.append('entry.1045781291', formData.email);
+        form.append('entry.1166974658', formData.phone);
+        form.append('entry.1065046570', formData.postalCode);
+        form.append('entry.839337160', formData.comments);
+        form.append('entry.1917880232', formData.collaborators);
+        form.append('entry.1481457655', formData.terms ? "He leído y acepto la Política de Privacidad: https://policies.google.com/privacy" : "");
+        form.append('emailAddress', formData.email);
+
+        try {
+            await fetch(googleFormUrl, {
+                method: 'POST',
+                mode: 'no-cors',
+                body: form
+            });
+
+            setFormData({
+                name: "",
+                email: "",
+                phone: "",
+                profile: "",
+                company: "",
+                collaborators: "",
+                otherCollaborator: "",
+                city: "",
+                postalCode: "",
+                comments: "",
+                terms: false,
+            });
+
+            toast.success(
+                <div>
+                    <p>Formulario enviado con éxito.</p>
+                    <p>
+                        Si deseas agilizar el proceso, puedes contactarnos directamente vía{" "}
+                        <button
+                            onClick={handleWhatsAppClick}
+                            className="underline text-blue-400"
+                        >
+                            WhatsApp
+                        </button>
+                        .
+                    </p>
+                </div>
+            );
+        } catch (error) {
+            toast.error('Error al enviar el mensaje. Por favor, intente nuevamente o comuníquese con nosotros por whatsapp.');
+        }
     };
 
     const collaboratorOptions = [
@@ -106,10 +152,12 @@ const ProfessionalForm = () => {
 
             {/* Colaboradores */}
             <FieldSetInput
+                type="radio"
                 legend={t("forms.professional.form.collaborators")}
                 options={collaboratorOptions}
                 optionsChecked={formData.collaborators}
-                onChange={toggleArrayValue}
+                onChange={(val) => handleChange("collaborators", val)}
+                required
             />
             <FormInput
                 type="text"
